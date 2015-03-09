7af71b36c98b503184553cdfe432ea8120263c65
Stephen Saucier <stephen@saucier.co>
2015-03-09 15:00:31 -0400

    Added conditional Aerobatic API proxying

--- client/assets/js/app.js
+++ client/assets/js/app.js
@@ -36,27 +36,28 @@
         url: "="
       },
       link: function(scope, element, attrs) {
+        // Use Aerobatic's caching if we're on that server
+        var urlApi = scope.url,
+          queryParams = {
+            cache: true
+          };
+
+        if (window.location.hostname.match('aerobaticapp')) {
+          queryParams = {
+            params: {
+              url: urlApi,
+              cache: 1,
+              ttl: 30000 // cache for 500 minutes
+            }
+          }
+          urlApi = '/proxy';
+        }
+
         var capitalize = $filter('capitalize');
-        $http.get(scope.url, { cache: true }).then(function(result) {
+        $http.get(urlApi, queryParams).then(function(result) {
           scope.property = capitalize(result.data[scope.prop]);
         }, function(err) {
           scope.property = "Unknown";
-        });
-      }
-    }
-  }])
-  .directive("getImage", ['$http', function($http) {
-    return {
-      template: "{{imageUrl}}",
-      scope: {
-        subject: "="
-      },
-      link: function(scope, element, attrs) {
-        $http.get('https://www.googleapis.com/customsearch/v1?cx=001000040755652345309%3Aosltt3fexvk&q='+encodeURIComponent(scope.subject)+'&imgSize=large&num=1&fileType=jpg&key=AIzaSyBDvUGYCJfOyTNoJzk-5P9vE-dllx-Wne4', { cache: true }).then(function(result) {
-          console.info(result.data);
-          scope.imageUrl = result.data.items[0].pagemap.cse_image.src;
-        }, function(err) {
-          scope.imageUrl = "Unknown";
         });
       }
     }
@@ -98,10 +99,27 @@
     $scope.id = ($state.params.id || '');
     $scope.page = ($state.params.p || 1);
 
+    // Use Aerobatic's caching if we're on that server
+    var urlApi = "http://swapi.co/api/"+multiple+"/"+$scope.id+"?page="+$scope.page,
+      queryParams = {
+        cache: true
+      };
+
+    if (window.location.hostname.match('aerobaticapp')) {
+      queryParams = {
+        params: {
+          url: urlApi,
+          cache: 1,
+          ttl: 30000 // cache for 500 minutes
+        }
+      }
+      urlApi = '/proxy';
+    }
+
     if ($scope.page == 1) {
       if ($scope.id != '') {
         // We've got a URL parameter, so let's get the single entity's data
-        $http.get("http://swapi.co/api/"+multiple+"/"+$scope.id, { cache: true })
+        $http.get(urlApi, queryParams)
           .success(function(data) {
             // The HTTP GET only works if it's referencing an ng-repeat'ed array for some reason...
             if (data.homeworld) data.homeworld = [data.homeworld];
@@ -109,7 +127,22 @@
             $scope[single] = data;
 
             // Get an image from a Google Custom Search (this API key only works on localhost & aerobaticapp.com)
-            $http.get('https://www.googleapis.com/customsearch/v1?cx=001000040755652345309%3Aosltt3fexvk&q='+encodeURIComponent(data.name)+'&imgSize=large&num=1&fileType=jpg&key=AIzaSyBDvUGYCJfOyTNoJzk-5P9vE-dllx-Wne4', { cache: true }).then(function(result) {
+            var googleUrl = 'https://www.googleapis.com/customsearch/v1?cx=001000040755652345309%3Aosltt3fexvk&q='+encodeURIComponent(data.name)+'&imgSize=large&num=1&fileType=jpg&key=AIzaSyBDvUGYCJfOyTNoJzk-5P9vE-dllx-Wne4',
+              googleParams = { cache: true };
+
+            if (window.location.hostname.match('aerobaticapp')) {
+              googleParams = {
+                params: {
+                  url: googleUrl,
+                  cache: 1,
+                  ttl: 300000 // cache for 5000 minutes
+                }
+              }
+              googleUrl = '/proxy';
+            }
+
+            $http.get(googleUrl, googleParams)
+            .then(function(result) {
               $scope.imageUrl = result.data.items[0].pagemap.cse_image[0].src;
             }, function(err) {
               $scope.imageUrl = "Unknown";
@@ -118,14 +151,16 @@
 
       } else {
         // We're on page 1, so thet next page is 2.
-        $http.get("http://swapi.co/api/"+multiple+"/", { cache: true }).success(function(data) {
+        $http.get(urlApi, queryParams)
+        .success(function(data) {
           $scope[multiple] = data;
           if (data['next']) $scope.nextPage = 2;
         });
       }
     } else {
       // If there's a next page, let's add it. Otherwise just add the previous page button.
-      $http.get("http://swapi.co/api/"+multiple+"/?page="+$scope.page, { cache: true }).success(function(data) {
+      $http.get(urlApi, queryParams)
+      .success(function(data) {
         $scope[multiple] = data;
         if (data['next']) $scope.nextPage = 1*$scope.page + 1;
       });
